847037875b5db2ea89dc7266a98a8dca6ca3e999
keplerridge <kepler.ridge@gmail.com>
2020-12-16 16:10:03 -0700

    finished app 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor () {
+    super();
+
+    this.state = {
+      names: ['Kepler', 'Bud', 'Cole', 'Tj', 'Cory'],
+      userInput: ''
+    }
+  }
+  
+  handleChange(e){
+    this.setState({userInput: e})
+  }
+
+
+  render(){
+    let displayNames = this.state.names.filter((e) => {
+      return e.includes(this.state.userInput)
+    })
+    .map((e, i) => {
+      return <h2 key={i}>{e}</h2>
+    })
+
+    return (
+      <section>
+        {displayNames}
+        <input value={this.state.userInput} onChange={e => this.handleChange(e.target.value)}/>
+      </section>
+    )
+  }
 }
 
 export default App;
