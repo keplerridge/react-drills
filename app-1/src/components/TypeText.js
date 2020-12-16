8dd5b3611321e7a6fc38b4a1604cedb5686f9d05
keplerridge <kepler.ridge@gmail.com>
2020-12-16 14:31:16 -0700

    finished first drill

---
+++ app-1/src/components/TypeText.js
@@ -0,0 +1,26 @@
+// import {Component} from 'react';
+
+// class TypeText extends Component {
+    
+//     constructor(){
+//         super();
+
+//         this.state = {
+//             userInput: '',
+//             displayText: ''
+//         }
+//     }
+    
+
+
+//     render(){
+//         return (
+//             <section>
+//                 <input />
+//                 <p>Display Text: </p>
+//             </section>     
+//         )
+//     }
+// }
+
+// export default TypeText;
