8b615925c89bab1d4e03345253ccf31672b1a527
keplerridge <kepler.ridge@gmail.com>
2020-12-16 17:18:21 -0700

    finished app 5

---
+++ app-5/src/component/Image.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Image extends Component{
+    render(){
+        return(
+            <div>
+                <img src='https://www.click2houston.com/resizer/RpgMd5a6ZnjFCMkUBA4lcHZUKJ0=/1280x720/smart/filters:format(jpeg):strip_exif(true):strip_icc(true):no_upscale(true):quality(65)/cloudfront-us-east-1.images.arcpublishing.com/gmg/WMVY6H37ERA53NELKEYPQ6BXDE.jpg' alt='Baby Yoda'></img>
+            </div>
+        )
+    }
+}
+
+export default Image;
